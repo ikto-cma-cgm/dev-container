e9c0c4441d801e5bb6263cafe4e8ac9fca9bf935
Michael Akbaraly <makbaraly@ikki-league.com>
2026-05-26 09:33:17 +0000

    feat(springboot-swagger): refactor template with dynamic package path, OpenAPI spec, and updated conventions

--- output/templates/springboot-swagger-service-template/skeleton/src/main/java/${{ values.packagePath }}/controller/HealthController.java
+++ output/templates/springboot-swagger-service-template/skeleton/src/main/java/${{ values.packagePath }}/controller/HealthController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class HealthController {
+public class HealthController {
 
     @GetMapping("/health")
-    ResponseEntity<String> health() {
+    public ResponseEntity<String> health() {
         return ResponseEntity.ok("OK");
     }
 
